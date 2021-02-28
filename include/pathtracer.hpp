d20c94aef71d1bda12e24e87d8d331f534afe66a
Thales Magalhaes <thls.mglhs@gmail.com>
2021-02-28 11:39:35 -0300

    Accout for absorbtion prob. in radiance estimation

--- include/pathtracer.hpp
+++ include/pathtracer.hpp
@@ -9,6 +9,19 @@ namespace drt {
 
 using Scene = std::vector<Shape*>;
 
+namespace internal {
+
+Vec3 sample_bxdf(BxDF *bxdf, Vec3 normal, Vec3 dir_in, double& pdf)
+{ return bxdf ? bxdf->sampler(normal, dir_in)->sample(pdf) : Vec3(0); }
+
+Var3 eval_bxdf(BxDF *bxdf, Vec3 normal, Vec3 dir_in, Vec3 dir_out)
+{ return bxdf ? (*bxdf)(normal, dir_in, dir_out) : Vec3(0); }
+
+Var3 emission(Emitter *emitter)
+{ return emitter ? emitter->emission() : Vec3(0); }
+
+} // namespace internal
+
 class Pathtracer {
 public:
     Pathtracer(double absorb, int min_bounces)
@@ -43,24 +56,11 @@ private:
 
     Var3 scatter(Scene& scene, RaycastHit& hit, Vec3 dir_in, int depth)
     {
-        Var3 brdf_value;
-        Var3 emission;
-        Vec3 dir_out;
-        Vec3 orig;
         double pdf;
-        if (hit.bxdf) {
-            auto sampler = hit.bxdf->sampler(hit.normal, -dir_in);
-            dir_out = sampler->sample(pdf);
-            orig = hit.point + 1e-3*dir_out;
-            brdf_value = hit.bxdf->operator()(hit.normal, -dir_in, dir_out);
-        } else {
-            brdf_value = Vec3(0);
-        }
-        if (hit.emitter) {
-            emission = hit.emitter ? hit.emitter->emission() : Vec3(0);
-        } else {
-            emission = Vec3(0);
-        }
+        Vec3 dir_out = internal::sample_bxdf(hit.bxdf, hit.normal, -dir_in, pdf);
+        Vec3 orig = hit.point + 1e-3*dir_out;
+        Var3 brdf_value = internal::eval_bxdf(hit.bxdf, hit.normal, -dir_in, dir_out);
+        Var3 emission = internal::emission(hit.emitter);
         Var3 radiance = trace(scene, orig, dir_out, depth+1);
         double cos_theta = dot(hit.normal, dir_out);
         return emission + brdf_value * radiance * cos_theta / pdf;
@@ -75,9 +75,10 @@ Var3 Pathtracer::trace(Scene& scene, Vec3 orig, Vec3 dir, int depth)
     if (depth >= m_min_bounces && random::uniform() < m_absorb) {
         return Vec3(0);
     }
+    double p = depth >= m_min_bounces ? (1 - m_absorb) : 1;
     RaycastHit hit;
     if (raycast(scene, orig, dir, hit)) {
-        return scatter(scene, hit, dir, depth);
+        return scatter(scene, hit, dir, depth) / p;
     } else {
         return Vec3(0);
     }
